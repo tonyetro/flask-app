f8e7d3a4d86a171ea8f0ce8931d407ea2c9a7a61
Tony G <ssj.tony89@gmail.com>
2021-01-20 03:20:52 +1100

    init commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import sqlite3
+from flask import Flask, render_template
+app = Flask(__name__)
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute(
+        'SELECT * FROM posts WHERE id = ?',
+    (post_id,)
+    ).fetchone()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
